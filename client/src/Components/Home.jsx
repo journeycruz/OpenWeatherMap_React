febc77c8212488222656ef753a7d2c303d3e4ed6
Journey Cruz <journey.cruz@gmail.com>
2021-08-23 09:12:30 -0500

    add Procfile for Heroku and center home content

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Home = () => {
 
    return (
-      <div>
+      <div className="container col-md-8 text-center">
          <div className='header'>
             <h2>Weather Forecast</h2>
          </div>
-
+         <br />
          <div className="instructions">
             <p>Enter a US zip code below to get the current weather conditions for that area.</p>
          </div>
