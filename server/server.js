64f708b2c02b5aab2880b0005c63244bb202d867
Journey Cruz <journey.cruz@gmail.com>
2021-08-23 09:15:56 -0500

    Update server.js for heroku deployment

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ app.use(express.json());
 
 require('./routes/api/routes')(app);
 
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
 app.listen(port, (err) => {
     if (err) { console.log(err) };
     console.log(`Server listening on port ${port}`);
