450ebfe285e0128a31d1eaccad650322a8e3bfe1
Journey Cruz <journey.cruz@gmail.com>
2021-08-26 18:03:48 -0500

    Move files around directory to test heroku and enable cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,9 +9,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(cors({
+    credentials: true,
+    origin: [
+        "http://localhost:3000",
+        "http://localhost:5000",
+        "https://openweathermap-react.herokuapp.com/",
+    ],
+}));
+
 require('./routes/api/routes')(app);
 
-app.use(express.static('public'));
+app.use(express.static('client/public'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '/client/public', 'index.html'));
