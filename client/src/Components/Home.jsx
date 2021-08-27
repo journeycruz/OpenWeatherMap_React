a5546242c34de97386f5879476ddba924c96e29e
Journey Cruz <journey.cruz@gmail.com>
2021-08-27 01:34:15 -0500

    add video bg on auto loop and change container styling in Home.jsx

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 
 const Home = () => {
-
    return (
-      <div className="container col-md-8 text-center">
-         <div className='header'>
-            <h2>Weather Forecast</h2>
-         </div>
-         <br />
-         <div className="instructions">
-            <p>Enter a US zip code below to get the current weather conditions for that area.</p>
-         </div>
-
-         <div className='cityInput'>
-	  <form method='POST' action='/search-location'>
-	     <input type='text' placeholder='Enter city..' name='city'/>
-	     <button>ENTER</button>
-	  </form>
+      <div>
+         <video className='videoTag' autoPlay loop muted>
+            <source src='../assets/home_bg.mp4' type='video/mp4' />
+         </video>
+         <div className="container col-md-7 text-center">
+            <div className='header'>
+               <h2>Weather Forecast</h2>
+            </div>
+            <br />
+            <div className="instructions">
+               <p>Enter a city name below to get the current weather conditions for that area.</p>
+            </div>
+            <div className='cityInput'>
+               <form method='POST' action='/search-location'>
+                  <input type='text' placeholder='Enter city..' name='city'/>
+                  <button>ENTER</button>
+               </form>
+            </div>
          </div>
       </div>
    )
