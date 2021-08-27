90201a90d1da2c475be71b3a955dd31ba5746d29
Journey Cruz <journey.cruz@gmail.com>
2021-08-27 16:32:03 -0500

    Style changes for screen sizes less than 600px

--- client/src/Components/Jumbotron.jsx
+++ client/src/Components/Jumbotron.jsx
@@ -3,13 +3,13 @@ import { Jumbotron } from 'reactstrap';
 
 const Hero = () => {
     return (
-        <div>
-            <div className="jumbotron justify-content-center">
+        <div id="home" className='vid-container m-0'>
+            <div className="justify-content-center m-0">
                 <Jumbotron>
                     <video className='videoTag' autoPlay loop muted playsinLine>
                         <source src='../assets/home_bg.mp4' type='video/mp4' />
                     </video>
-                    <div className="container hero col-md-7 text-center">
+                    <div className="container hero col-sm-7 text-center m-0">
                         <div className='header'>
                             <h2 className='display-5'>Welcome to my Weather App</h2>
                         </div>
