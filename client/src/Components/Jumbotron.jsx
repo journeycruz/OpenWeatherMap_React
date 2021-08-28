8c451df9341e3d149bb4d32b8c17b8096043ee71
Journey Cruz <journey.cruz@gmail.com>
2021-08-27 19:31:21 -0500

    Style changes

--- client/src/Components/Jumbotron.jsx
+++ client/src/Components/Jumbotron.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                     <video className='videoTag' autoPlay loop muted playsinLine>
                         <source src='../assets/home_bg.mp4' type='video/mp4' />
                     </video>
-                    <div className="container hero col-md-5 text-center m-0">
+                    <div className="container hero col-md-7 text-center m-0">
                         <div className='header'>
                             <h2 className='display-5'>Welcome to my Weather App</h2>
                         </div>
